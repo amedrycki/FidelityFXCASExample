13ffca86f595f7f6822880870b6fe6112848b2e1
Andy <amedrycki@users.noreply.github.com>
2020-04-07 15:25:37 +0200

    Added empty project files

---
+++ Source/FidelityFXCASExample/FidelityFXCASExampleGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FidelityFXCASExampleGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FIDELITYFXCASEXAMPLE_API AFidelityFXCASExampleGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
